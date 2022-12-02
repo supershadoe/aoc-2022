de5e51296c5b2777afb23e95d9e3c56eb75eee13
supershadoe <supershadoe@proton.me>
2022-12-02 11:27:26 +0530

    d2p2

---
+++ src/day2p2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+extern int errno;
+
+int main() {
+    FILE *f;
+    char choices[4];
+    int sum = 0;
+    f = fopen("input/day2.txt", "r");
+    if (f == NULL) {
+        perror("Cannot open input/day2.txt");
+        return errno;
+    }
+
+    // lol hard coded solution
+    while ( ! feof(f) ) {
+        fgets(choices, 4, f);
+        if( ! strcmp(choices, "A X")) sum += 3;
+        else if ( ! strcmp(choices, "A Y")) sum += 4;
+        else if ( ! strcmp(choices, "A Z")) sum += 8;
+        else if ( ! strcmp(choices, "B X")) sum += 1;
+        else if ( ! strcmp(choices, "B Y")) sum += 5;
+        else if ( ! strcmp(choices, "B Z")) sum += 9;
+        else if ( ! strcmp(choices, "C X")) sum += 2;
+        else if ( ! strcmp(choices, "C Y")) sum += 6;
+        else if ( ! strcmp(choices, "C Z")) sum += 7;
+    }
+    fclose(f);
+    printf("The total score that can be obtained from this strategy guide is %d.\n", sum);
+    return 0;
+}
