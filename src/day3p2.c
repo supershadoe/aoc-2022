9cc7c35bbd6aa13b87d921e052c16b19bbb3e792
supershadoe <supershadoe@proton.me>
2022-12-03 22:24:36 +0530

    d3p2

---
+++ src/day3p2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+extern int errno;
+
+int main() {
+    FILE *f;
+    char items[3][60], *match;
+    int sum = 0;
+    f = fopen("input/day3.txt", "r");
+    if (f == NULL) {
+        perror("Cannot open input/day3.txt");
+        return errno;
+    }
+
+    while (
+        fgets(items[0], 60, f),
+        fgets(items[1], 60, f),
+        fgets(items[2], 60, f)
+    ) {
+        match = strpbrk(items[0], items[1]);
+        while(
+            match != NULL
+            && match < &items[0][59]
+            && strchr(items[2], *match) == NULL
+        ) {
+            match = strpbrk(match+1, items[1]);
+        }
+        if (match != NULL)
+            if (*match >= 'A' && *match <= 'Z')
+                sum += *match - 38;
+            else if (*match >= 'a' && *match <= 'z')
+                sum += *match - 96;
+            else
+                printf("Invalid input.\n"); // never executed
+        else
+            printf("The strings had nothing in common.\n"); // never executed
+    }
+    fclose(f);
+    printf("The sum of the priorities of the item types is %d.\n", sum);
+
+    return 0;
+}
